d44a7544e799b2b6fe33e6e003a69fe4a7385995
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-04-22 09:44:51 +0530

    Refactor Main class to extend LinkedList and reorganize class structure

--- DSA/LinkedList/Main.java
+++ DSA/LinkedList/Main.java
@@ -1,5 +1,21 @@
-public class Main{
+
+public class Main extends LinkedList {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+}
+
+class LinkedList extends Node {
+
+    private int first;
+    private int last;
+
+    //METHODS:
+}
+
+class Node {
+
+    private int data;
+    private Node next;
 }
