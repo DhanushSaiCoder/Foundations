2ec23a1188ab88834c081167e50bc43d907887f8
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-04-22 13:43:12 +0530

    Add RemoveNthFromEnd class and initial LinkedList structure

---
+++ DSA/LinkedList/practice problems/RemoveNthFromEnd.java
@@ -0,0 +1,20 @@
+
+public class RemoveNthFromEnd extends LinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
+
+class LinkedList {
+
+    private class Node {
+
+        private int value;
+        private Node next;
+    }
+    private Node first;
+    private Node last;
+
+}
