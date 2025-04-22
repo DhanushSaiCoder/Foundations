a9ce82cd2017b15dc7d9fd6ddecff87647d3aa5f
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-04-22 09:12:45 +0530

    Add .gitignore and initial LinkedList implementation

---
+++ DSA/LinkedList/list.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class list {
+    public static void main(String args[]) {
+        LinkedList list = new LinkedList();
+        list.addFirst(01);
+        list.addLast(02);
+        
+    }
+}
