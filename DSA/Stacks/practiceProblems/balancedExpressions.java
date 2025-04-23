e4b8794ce0a15f1697bd55bc5e6740a37123ee19
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-04-23 12:35:00 +0530

    Refactor balancedExpressions class to improve structure and readability

--- DSA/Stacks/practiceProblems/balancedExpressions.java
+++ DSA/Stacks/practiceProblems/balancedExpressions.java
@@ -1,5 +1,68 @@
-public class balancedExpressions{
+
+import java.util.Stack;
+
+public class balancedExpressions {
+
     public static void main(String[] args) {
-        
+        String expression = "{[23e^2 + (pi * 71e)]/(2x^2 + 13x + 7)}";
+        System.out.println("\nIs Balanced: " + isBalanced(expression).toString());
     }
+
+    static Boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : expression.toCharArray()) {
+            if (isOpening(ch)) {
+                stack.push(findClosingPair(ch));
+            } else if (isClosing(ch)) {
+                if (stack.isEmpty() || ch != stack.pop()) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    static Boolean isOpening(char ch) {
+        char openings[] = {'{', '[', '(', '<'};
+        for (char c : openings) {
+            if (c == ch) {
+                System.out.print(ch);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static Boolean isClosing(char ch) {
+        char closings[] = {'}', ']', ')', '>'};
+        for (char c : closings) {
+            if (c == ch) {
+                System.out.print(ch);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static Character findClosingPair(char ch) {
+        switch (ch) {
+            case '{' -> {
+                return '}';
+            }
+            case '[' -> {
+                return ']';
+            }
+            case '<' -> {
+                return '>';
+            }
+            case '(' -> {
+                return ')';
+            }
+            default ->
+                throw new AssertionError();
+        }
+    }
+
 }
