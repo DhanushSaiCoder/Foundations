8549df394cc8ec189d1e0e506b2cdaf72f4b3f41
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-04-23 11:42:46 +0530

    Refactor reverseAString class to improve readability and structure

--- DSA/Stacks/practiceProblems/reverseAString.java
+++ DSA/Stacks/practiceProblems/reverseAString.java
@@ -1,5 +1,22 @@
-public class reverseAString{
-    public static void main(String args[]){
-        
+
+import java.util.Stack;
+
+public class reverseAString {
+
+    public static void main(String args[]) {
+        Stack<Character> stack = new Stack<>();
+        String str = "Dhanush";
+        int i = 0;
+        System.out.println(str);
+        do {
+            stack.push(str.charAt(i));
+            i++;
+        } while (i < str.length());
+        str="";
+        for (int n = 0; !stack.isEmpty(); n++) {
+            str += stack.pop();
+        }
+        System.out.println(str);
+
     }
 }
