4feccb0ee7f09346baf38bf60894b1eb569fb9b3
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-04-24 13:00:41 +0530

    Implement main method in ReverseAQueue to demonstrate queue operations

--- DSA/Queue/exercises/ReverseAQueue.java
+++ DSA/Queue/exercises/ReverseAQueue.java
@@ -1,5 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class ReverseAQueue{
     public static void main(String[] args) {
-        System.out.println("helloWithQueue");
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        
+        System.out.println(queue.remove());
+
     }
 }
