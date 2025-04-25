cec2e8b6811dc168028700a995b5da28f027a81c
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-04-25 09:36:32 +0530

    Implement initial version of the HashMap functionality in the Implementation class

---
+++ DSA/HashMaps/Implementation.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class Implementation {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Dhanush");
+        map.put(2, "Sai");
+        map.put(3, "Nayak");
+        System.out.println(map);
+    }
+
+    public static void pln(String msg) {
+        System.out.println(msg);
+    }
+}
