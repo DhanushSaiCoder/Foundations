890ed570110ebb5be143c6948e05bb7bbd67519f
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-04-25 10:03:22 +0530

    Add FirstNonRepeated class to find the first non-repeated character in a string

---
+++ DSA/HashMaps/FirstNonRepeated.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class FirstNonRepeated {
+
+    public static void main(String[] args) {
+        String str = "I am Getting Bored";
+        System.out.println(check(str));
+    }
+
+    public static Character check(String str) {
+        str = str.trim().toLowerCase().strip();
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            if (map.containsKey(ch)) {
+                // get value of the key 'ch',
+                // increment it
+                map.put(ch, map.get(ch) + 1);
+                continue;
+            }
+            map.put(ch, 1);
+        }
+
+        if(map.containsValue(1)) {
+            for(Character key : map.keySet()) {
+                if (map.get(key) == 1) {
+                    return key;
+                }
+            }
+        }
+        return ' ';
+    }
+}
